ac4c642ce29ec7c22d7eece45bdb842a2ff495ae
NetanelYaso <netanelyaso12@gmail.com>
2022-06-15 18:12:16 +0300

    switch-case-1

---
+++ main.js
@@ -0,0 +1,87 @@
+// function someFunc(){
+//     let number = +document.getElementById("input_number").value
+//     switch(number){
+//         case 3:
+//             console.log("hello");
+//             return;
+//         case 5:
+//             console.log("goodbye"); 
+//             return;
+//         case 7:
+//             console.log("thank you"); 
+//             return;
+//         default:
+//             console.log("there is no fit measege");             
+//     }
+// }
+
+// function myName(){
+//     let name = document.getElementById("name_input").value;
+//     switch(name){
+//         case "jacob":
+//             return my_p.innerHTML = "jacob";
+//         case "nathan":
+//             return my_p.innerHTML = name.toUpperCase();
+//         case "DALYA":
+//             return my_p.innerHTML = name.toLowerCase();
+//         default:
+//             my_p.innerHTML = "there is no fit measege"
+//     }
+// }
+
+// function mathProbleme(){
+//     switch(math_sign.value){
+//         case "+":
+//            return result.innerHTML = +my_number1.value + +my_number2.value;
+//         case "-":
+//            return result.innerHTML = +my_number1.value- +my_number2.value;
+//         case "*":
+//            return result.innerHTML = +my_number1.value* +my_number2.value;
+//         case "/":
+//            return result.innerHTML = +my_number1.value/ +my_number2.value;
+//         default:
+//             break;
+//     }
+// }
+
+// function printTextAndColor(){
+//     let number = +document.getElementById("my_number").value;
+//     switch(number){
+//         case number:
+//         p1.innerHTML = text_input.value;
+//         p1.style = "color:"+ color1.value + ";" + "font-Size:"+ number+"px;"
+//     }
+// }
+
+// function elementPosition(){
+//     let mySelect = document.getElementById("select_id").value;
+//     let elementCircle = document.getElementById("my_div")
+//     switch(mySelect){
+//         case "top-left":
+//             return elementCircle.style = "border-radius:50%; height:100px; width:100px; background: "+color_input.value+";"
+//         case "top-right":
+//             return elementCircle.style = "float:right; border-radius:50%; height:100px; width:100px; background: "+color_input.value+";"
+//         case "bottom-left":
+//             return elementCircle.style = "position:fixed; bottom:0; border-radius:50%; height:100px; width:100px; background: "+color_input.value+";"
+//         case "bottom-right":
+//             return elementCircle.style = "position:absolute; bottom:0; right:0; border-radius:50%; height:100px; width:100px; background: "+color_input.value+";"
+//         default:
+//             break;
+//     }
+// }
+
+function jsCalculator(){
+    let myNumber=1;
+    switch(myNumber){
+        case "+":
+            
+        case "-":
+
+        case "*":
+
+        case "/":
+
+        default:
+            break;
+    }
+}
